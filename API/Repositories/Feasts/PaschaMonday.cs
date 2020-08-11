62aab78e8011f6beb67ce52963b4bdf06119df23
Pierre SAID <said.pierre.emler@gmail.com>
2020-08-11 22:16:22 +0200

    Pascha Monday  ok

--- API/Repositories/Feasts/PaschaMonday.cs
+++ API/Repositories/Feasts/PaschaMonday.cs
@@ -19,7 +19,7 @@ namespace Katameros.Repositories
                     subSections = new List<SubSection>() {
                         await _readingsRepository.MakeOldTestament("1.1:1-31*@+1.2:1-3"),
                         await _readingsRepository.MakeOldTestament("23.5:1-9"),
-                        await _readingsRepository.MakeOldTestament("73.1:1-24"),
+                        await _readingsRepository.MakeOldTestament("73.1:1-19"),
                         await _readingsRepository.MakePsalmAndGospel("19.72:18-19", "41.11:12-24"),
                     }
                 },
@@ -47,8 +47,8 @@ namespace Katameros.Repositories
                     subSections = new List<SubSection>() {
                         await _readingsRepository.MakeOldTestament("1.2:15-25*@+1.3:1-24"),
                         await _readingsRepository.MakeOldTestament("23.40:1-5"),
-                        await _readingsRepository.MakeOldTestament("20.10:1-9"),
-                        await _readingsRepository.MakePsalmAndGospel("19.65:6,5", "40.21:23-27"),
+                        await _readingsRepository.MakeOldTestament("20.1:1-9"),
+                        await _readingsRepository.MakePsalmAndGospel("19.65:5,4", "40.21:23-27"),
                     }
                 },
                 new Section
@@ -57,17 +57,10 @@ namespace Katameros.Repositories
                     subSections = new List<SubSection>() {
                         await _readingsRepository.MakeOldTestament("23.50:1-3"),
                         await _readingsRepository.MakeOldTestament("73.1:20-30"),
-                        await _readingsRepository.MakePsalmAndGospel("19.13:4-5", "43.8:51-59"),
+                        await _readingsRepository.MakePsalmAndGospel("19.13:3-4", "43.8:51-59"),
                     }
                 }
             };
-            //dayReadings.Sections.Add(firstHour);
-            //var liturgy = await _readingsRepository.MakeLitugy("58.9:11-28", "60.4:1-11", "44.28:11-31", null, null);
-            //liturgy.subSections.Add(await _readingsRepository.MakePsalmAndGospel("19.81:3,1,2", "40.21:1-17"));
-            //liturgy.subSections.Add(await _readingsRepository.MakePsalmAndGospel(null, "41.11:1-11"));
-            //liturgy.subSections.Add(await _readingsRepository.MakePsalmAndGospel(null, "42.19:29-48"));
-            //liturgy.subSections.Add(await _readingsRepository.MakePsalmAndGospel("19.65:1-2", "43.12:12-19"));
-            //dayReadings.Sections.Add(liturgy);
             return dayReadings;
         }
     }
