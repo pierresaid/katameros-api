c310c21d30da76e3c4e791867099ba0eeff39479
Pierre Said <ext.pierre.said@bollore.com>
2021-04-28 14:37:28 +0200

    Fix lazarus

--- API/Factories/Feasts/LazarusSaturday.cs
+++ API/Factories/Feasts/LazarusSaturday.cs
@@ -14,7 +14,7 @@ namespace Katameros.Repositories
                 Title = await GetFeastTranslation(Feast.LazarusSaturday),
                 Sections = new List<Section>
                 {
-                    await _readingsRepository.MakeMatins("19.30:3-11", "42.18:35-43", "1.49:1-28*@+23.40:9-31*@+36.3:14-20*@+38.9:9-15"),
+                    await _readingsRepository.MakeMatins("19.30:3,11", "42.18:35-43", "1.49:1-28*@+23.40:9-31*@+36.3:14-20*@+38.9:9-15"),
                     await _readingsRepository.MakeLitugy("46.2:1-8", "60.1:25*@+60.2:1-6", "44.27:38-44*@+44.28:1-10", "19.129:8,2", "43.11:1-45")
                 }
             };
