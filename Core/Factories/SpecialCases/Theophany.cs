acb8b32db1bf77d7c913d08234a87a65658f0a63
Pierre Said <said.pierre.emler@gmail.com>
2026-01-15 20:40:46 +0100

    Fix Theophany Paramoun days

--- Core/Factories/SpecialCases/Theophany.cs
+++ Core/Factories/SpecialCases/Theophany.cs
@@ -68,7 +68,7 @@ public class Theophany : ISpecialCase
             {
                 await _readingsRepository.MakeVespers("19.42:2,5", "40.4:12-22"),
                 await _readingsRepository.MakeMatins("19.42:7,8", "43.3:22-29"),
-                await _readingsRepository.MakeLitugy("46.1:1-17", "61.1:12-19", "44.16:25-34", "19.44:3,4", "42.3:1-18")
+                await _readingsRepository.MakeLitugy("46.1:1-17", "61.1:12-19", "44.16:25-34", "19.45:2", "42.3:1-18")
             }
         };
     }
