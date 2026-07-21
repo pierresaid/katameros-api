af212f5d335f86988f6026398c362adee2d41c78
Pierre SAID <pierre.said@expaceo.com>
2026-07-21 11:54:44 +0200

    Synaxarium NL

--- Core/Repositories/ReadingsRepository.cs
+++ Core/Repositories/ReadingsRepository.cs
@@ -212,7 +212,7 @@ public class ReadingsRepository
         subSections.Add(await MakePauline(paulineRef));
         subSections.Add(await MakeCatholic(catholicRef));
         subSections.Add(await MakeActs(actsRef));
-        if (_context.LanguageId == 1 || _context.LanguageId == 2 || _context.LanguageId == 3 || _context.LanguageId == 4)
+        if (_context.LanguageId == 1 || _context.LanguageId == 2 || _context.LanguageId == 3 || _context.LanguageId == 4 || _context.LanguageId == 9)
             subSections.Add(await MakeSynaxarium(_context.CopticDate.Day, _context.CopticDate.Month));
         if (gospelRef != null)
             subSections.Add(await MakePsalmAndGospel(psalmRef, gospelRef));
